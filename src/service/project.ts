10b56482442bbc5d196e3acacc359adbebd5dd30
tobi akinyemi <tobi@akinyemi.org>
2024-08-15 12:37:12 +0100

    project updates

--- src/service/project.ts
+++ src/service/project.ts
@@ -56,8 +56,43 @@
 // );
 
 import {StudioProject} from "../model/project.js";
+import {execute, findOneField} from "./database.js";
+import {UUID} from "../model/id.js";
 
 export async function insertProject(project: StudioProject) {
+    // insert project, then the tracks, then the clips
+    const projectId = await findOneField<UUID>(`insert into studio_project (name, tempo, grid, snap_to_grid, play_head, solo_target)
+ values ($1, $2, $3, $4, $5, $6) returning id`,
+        project.name, project.tempo, project.grid, project.snapToGrid, project.playHead, project.soloTarget);
 
+    for (const track of project.tracks) {
+        const trackId = await findOneField<UUID>(`insert into project_track (name, pan, volume, muted, project_id)
+    values ($1, $2, $3, $4, $5) returning id`,
+            track.name, track.pan, track.volume, track.muted, projectId);
 
+        for (const clip of track.clips) {
+            const source = clip.source;
+            const isGenerationSource = "generationJobId" in source;
+            await execute(`insert into project_track_clip (colour_h, colour_s, colour_l, audio_id, sample_id, generation_job_id, generation_output_name, pitch_offset, reversed, name, speed, position, cut_start, cut_end, volume, pan, muted, track_id)
+    values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18)`,
+                clip.color?.hue,
+                clip.color?.saturation,
+                clip.color?.lightness,
+                source.audioId,
+                isGenerationSource ? null : source.sampleId,
+                isGenerationSource ? source.generationJobId : null,
+                isGenerationSource ? source.outputName : null,
+                clip.pitchOffset,
+                clip.reversed,
+                clip.name,
+                clip.speed,
+                clip.position,
+                clip.cutStart,
+                clip.cutEnd,
+                clip.volume,
+                clip.pan,
+                clip.muted,
+                trackId);
+        }
+    }
 }
